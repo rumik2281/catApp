e0f723d608a59e8a113d2b8b19b4930af5295519
Kiril Rumyantsev <kiril.rumyantsev228@gmail.com>
2022-04-12 17:54:52 +0300

    animation

--- app/src/main/java/com/example/catapp/MainActivity.java
+++ app/src/main/java/com/example/catapp/MainActivity.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button feedButton;
     int satiety;
+    ImageView myImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myImageView = findViewById(R.id.imageView);
+
+        final Animation animationRotateCenter = AnimationUtils.loadAnimation(
+                this, R.anim.rotate_center);
         feedButton = (Button) findViewById(R.id.feedButton);
         feedButton.setOnClickListener(new View.OnClickListener() {
 
@@ -24,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 satiety++;
                 ((TextView) findViewById(R.id.satietyCounter)).setText(Integer.toString(satiety));
+                if (satiety % 15 == 0) {
+                    myImageView.startAnimation(animationRotateCenter);
+                }
             }
         });
     }
