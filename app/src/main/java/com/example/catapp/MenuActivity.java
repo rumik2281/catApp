086fd307bfa0c2c6aaca73034e35e480627af79a
Kiril Rumyantsev <kiril.rumyantsev228@gmail.com>
2022-04-13 20:35:27 +0300

    menu

--- app/src/main/java/com/example/catapp/MenuActivity.java
+++ app/src/main/java/com/example/catapp/MenuActivity.java
@@ -1,16 +1,29 @@
 package com.example.catapp;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MenuActivity extends Activity {
+    Button ftcButton;
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_main);
+        ftcButton = (Button) findViewById(R.id.ftcButton);
+        Intent myIntent = new Intent(this, MainActivity.class);
+        ftcButton.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                startActivity(myIntent);
+            }
+        });
         };
     }
