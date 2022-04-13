b828de21d6e88a0bb1864cf7c0b9b4c6fab780d7
rumik2281 <kiril.rumyantsev228@gmail.com>
2022-04-14 00:14:19 +0300

    google auth

--- app/src/main/java/com/example/catapp/MenuActivity.java
+++ app/src/main/java/com/example/catapp/MenuActivity.java
@@ -8,13 +8,64 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.SignInAccount;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class MenuActivity extends Activity {
     Button ftcButton;
+    SignInButton signButton;
+    GoogleSignInClient nGoogleSignInClient;
+    TextView googleName;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    public void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            System.out.println(account.toString());
+            googleSignInOk(account);
+        } catch(Exception e) {
+            System.out.println(e.toString());
+            googleSignInOk(null);
+        }
+    }
+
+    public void googleSignInOk(GoogleSignInAccount account) {
+        if (account == null) {
+            googleName.setText("Error");
+        } else {
+            googleName.setText(account.getDisplayName());
+        }
+    }
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_main);
+        googleName = findViewById(R.id.nameText);
+        signButton = (SignInButton) findViewById(R.id.sgnButton);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        nGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+        signButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = nGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 100);
+            }
+        });
         ftcButton = (Button) findViewById(R.id.ftcButton);
         Intent myIntent = new Intent(this, MainActivity.class);
         ftcButton.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +76,6 @@ public class MenuActivity extends Activity {
                 startActivity(myIntent);
             }
         });
+
         };
     }
